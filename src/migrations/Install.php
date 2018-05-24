8316731db02ed3fd1550b3f6e58ddd4ff241f996
Josh Crawford <joshua@sgroup.com.au>
2018-05-24 16:26:29 +0930

    Cleanup install class

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace verbb\cpnav\migrations;
 
 use Craft;
@@ -7,145 +6,64 @@ use craft\db\Migration;
 
 class Install extends Migration
 {
-
     // Public Properties
     // =========================================================================
-
-    /**
-     * @var string The database driver to use
-     */
-    public $driver;
 
 
     // Public Methods
     // =========================================================================
 
-    /**
-     * @return bool
-     */
-    public function safeUp(): bool
+    public function safeUp()
     {
-        $this->driver = Craft::$app->getConfig()->getDb()->driver;
-        if ($this->createTables()) {
-            $this->addForeignKeys();
-
-            // Refresh the db schema caches
-            Craft::$app->db->schema->refresh();
-        }
-
-        return true;
+        $this->createTables();
+        $this->addForeignKeys();
     }
 
-    /**
-     * @return bool
-     */
-    public function safeDown(): bool
+    public function safeDown()
     {
-        $this->driver = Craft::$app->getConfig()->getDb()->driver;
         $this->removeTables();
-
-        return true;
     }
 
-
-    // Protected Methods
-    // =========================================================================
-
-    /**
-     * Creates the tables needed for the Records used by the plugin
-     *
-     * @return bool
-     */
-    protected function createTables(): bool
+    public function createTables()
     {
-        $tablesCreated = false;
+        $this->createTable('{{%cpnav_layout}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(255),
+            'isDefault' => $this->boolean()->notNull()->defaultValue(false),
+            'permissions' => $this->text(),
+            'dateCreated' => $this->dateTime()->notNull(),
+            'dateUpdated' => $this->dateTime()->notNull(),
+            'uid' => $this->uid(),
+        ]);
 
-        // cpnav_layout table
-        $tableSchema = Craft::$app->db->schema->getTableSchema('{{%cpnav_layout}}');
-        if ($tableSchema === null) {
-            $tablesCreated = true;
-            $this->createTable(
-                '{{%cpnav_layout}}',
-                [
-                    'id'          => $this->primaryKey(),
-
-                    // Custom columns in the table
-                    'name'        => $this->string(255),
-                    'isDefault'   => $this->boolean()->notNull()->defaultValue(false),
-                    'permissions' => $this->text(),
-
-                    // Yii stuff
-                    'dateCreated' => $this->dateTime()->notNull(),
-                    'dateUpdated' => $this->dateTime()->notNull(),
-                    'uid'         => $this->uid(),
-                ]
-            );
-        }
-
-        // cpnav_navigation table
-        $tableSchema = Craft::$app->db->schema->getTableSchema('{{%cpnav_navigation}}');
-        if ($tableSchema === null) {
-            $tablesCreated = true;
-            $this->createTable(
-                '{{%cpnav_navigation}}',
-                [
-                    'id'          => $this->primaryKey(),
-
-                    // Custom columns in the table
-                    'layoutId'    => $this->integer()->notNull(),
-                    'handle'      => $this->string(255),
-                    'prevLabel'   => $this->string(255),
-                    'currLabel'   => $this->string(255),
-                    'enabled'     => $this->boolean()->notNull()->defaultValue(true),
-                    'order'       => $this->integer()->defaultValue(0),
-                    'prevUrl'     => $this->string(255),
-                    'url'         => $this->string(255),
-                    'icon'        => $this->string(255),
-                    'customIcon'  => $this->string(255),
-                    'manualNav'   => $this->boolean()->notNull()->defaultValue(false),
-                    'newWindow'   => $this->boolean()->notNull()->defaultValue(false),
-
-                    // Yii stuff
-                    'dateCreated' => $this->dateTime()->notNull(),
-                    'dateUpdated' => $this->dateTime()->notNull(),
-                    'uid'         => $this->uid(),
-                ]
-            );
-        }
-
-        return $tablesCreated;
+        $this->createTable('{{%cpnav_navigation}}', [
+            'id' => $this->primaryKey(),
+            'layoutId' => $this->integer()->notNull(),
+            'handle' => $this->string(255),
+            'prevLabel' => $this->string(255),
+            'currLabel' => $this->string(255),
+            'enabled' => $this->boolean()->notNull()->defaultValue(true),
+            'order' => $this->integer()->defaultValue(0),
+            'prevUrl' => $this->string(255),
+            'url' => $this->string(255),
+            'icon' => $this->string(255),
+            'customIcon' => $this->string(255),
+            'manualNav' => $this->boolean()->notNull()->defaultValue(false),
+            'newWindow' => $this->boolean()->notNull()->defaultValue(false),
+            'dateCreated' => $this->dateTime()->notNull(),
+            'dateUpdated' => $this->dateTime()->notNull(),
+            'uid' => $this->uid(),
+        ]);
     }
 
-    /**
-     * Creates the foreign keys needed for the Records used by the plugin
-     *
-     * @return void
-     */
-    protected function addForeignKeys()
+    public function addForeignKeys()
     {
-        // cpnav_navigation table
-        $this->addForeignKey(
-            $this->db->getForeignKeyName('{{%cpnav_navigation}}', 'layoutId'),
-            '{{%cpnav_navigation}}',
-            'layoutId',
-            '{{%cpnav_layout}}',
-            'id',
-            'CASCADE',
-            'CASCADE'
-        );
+        $this->addForeignKey(null, '{{%cpnav_navigation}}', ['layoutId'], '{{%cpnav_layout}}', ['id'], 'CASCADE', null);
     }
 
-    /**
-     * Removes the tables needed for the Records used by the plugin
-     *
-     * @return void
-     */
-    protected function removeTables()
+    public function removeTables()
     {
-        // delete cpnav_navigation table first because of foreign key constraint
         $this->dropTableIfExists('{{%cpnav_navigation}}');
-
-        // cpnavs_layout table
         $this->dropTableIfExists('{{%cpnav_layout}}');
     }
 }
