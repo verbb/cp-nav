e22dd5b92dfc1707134092c948b185ef876549fa
Josh Crawford <joshua@sgroup.com.au>
2018-07-18 23:38:33 +1000

    Add flag for showing nav preview only when editing

--- src/services/CpNavService.php
+++ src/services/CpNavService.php
@@ -43,10 +43,12 @@ class CpNavService extends Component
 
             // If we're passing in a layoutId param, we're likely on the CP Nav settings page
             // so we want to force the particular layout we're on to the selected one
+            $editing = false;
             $layoutId = Craft::$app->getRequest()->getParam('layoutId');
 
             if ($layoutId) {
                 $layout = CpNav::$plugin->layoutService->getById($layoutId);
+                $editing = true;
             }
 
             // Its pretty annoying, but each load of the CP, we need to check if the stored
@@ -60,7 +62,7 @@ class CpNavService extends Component
                 $manualNavs = CpNav::$plugin->navigationService->getAllManual($layout->id, 'handle');
 
                 // Something has changed - either added or deleted. Re-generate the menu
-                if (count($nav) != count($manualNavs)) {
+                if ((count($nav) != count($manualNavs)) && !$editing) {
                     $this->regenerateNav($layout->id, $manualNavs, $nav);
 
                     // We've either deleted/removed an element = fetch again
