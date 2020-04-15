6ac697e6438a6245a7a3e6658477041f007399cb
Josh Crawford <joshua@sgroup.com.au>
2020-04-15 10:05:28 +1000

    Add checks to prevent multiple nav items being added in some instances

--- src/services/PendingNavigationsService.php
+++ src/services/PendingNavigationsService.php
@@ -29,6 +29,26 @@ class PendingNavigationsService extends Component
 
     public function set($pluginNavItem)
     {
+        $exists = false;
+
+        // Probably a better way to do this, but can't be bothered adding a new column to the table
+        $navItems = (new Query())
+            ->select(['pluginNavItem'])
+            ->from('{{%cpnav_pending_navigations}}')
+            ->column();
+
+        foreach ($navItems as $navItem) {
+            $json = Json::decode($navItem);
+
+            if ($json['url'] === $pluginNavItem['url']) {
+                $exists = true;
+            }
+        }
+
+        if ($exists) {
+            return;
+        }
+
         Craft::$app->getDb()->createCommand()
             ->insert('{{%cpnav_pending_navigations}}', ['pluginNavItem' => Json::encode($pluginNavItem)])
             ->execute();
